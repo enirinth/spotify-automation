88f4dfd66672263c569649f8f0bbc35d6b1aed7e
Aaron <enirinth@umich.edu>
2016-07-21 02:20:35 -0700

    Update get_access_token.js

--- get_access_token.js
+++ get_access_token.js
@@ -1,7 +1,6 @@
 /**
- * This is an example of a basic node.js script that performs
- * the Authorization Code oAuth2 flow to authenticate against
- * the Spotify Accounts.
+ * This is mostly a fork of spotify auth flow example code
+ * with some modifications on scopes
  *
  * For more information, read
  * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
